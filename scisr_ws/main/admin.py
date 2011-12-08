9d9ac24e6c1920345dff0edd44009ee5f38d4516
Uri Lavi <uri.i.lavi@gmail.com>
2011-12-08 19:49:37 +0200

    -- : --

---
+++ scisr_ws/main/admin.py
@@ -0,0 +1,4 @@
+from main.models import Event
+from django.contrib import admin
+
+admin.site.register(Event)
--- scisr_ws/main/models.py
+++ scisr_ws/main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description=models.TextField()
+    time_start=models.DateTimeField()
+    time_end=models.DateTimeField()
